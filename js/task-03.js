09d5ae7225262d5cc85f142479c435560bd20dc3
Alex-Stupak <stupakalexx@gmail.com>
2021-11-08 11:46:58 +0200

    Переделал дз 3

--- js/task-03.js
+++ js/task-03.js
@@ -2,32 +2,24 @@ const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'White and Black Long Fur Cat',
-    width: 1080
   },
   {
     url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-    width: 1080
   },
   {
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
-    width: 1080
   },
 ];
 
-const ulItemElmap = document.querySelector('.gallery')
-const makeImages = options => {
-  return options.map(option => {
-  const liItemEl = document.createElement('li');
-  liItemEl.classList.add('img')
-  const imageEl = document.createElement('img');
-  imageEl.src = option.url;
-  imageEl.alt = option.alt;
-  imageEl.width = option.width;
-  liItemEl.appendChild(imageEl)
-  return liItemEl;
-})
-}
-const ulElements = makeImages(images);
-ulItemElmap.append(...ulElements)
+const liImages = images
+  .map(pic => `<li><img src=${pic.url} alt=${pic.alt}></li>`)
+  .join('');
+
+const ulImages = document.querySelector('.gallery');
+ulImages.style.listStyleType = 'none';
+ulImages.style.display = 'flex';
+ulImages.style.flexDirection = 'column';
+ulImages.style.width = '1080px';
+ulImages.insertAdjacentHTML('beforeend', liImages);
